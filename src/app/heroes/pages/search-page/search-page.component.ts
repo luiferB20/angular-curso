1731b389e185da6a38b1707332b8d325081579fc
luis <luisfernandobuelvastordecilla@gmail.com>
2024-05-14 00:06:16 -0500

    buscador

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-search-page',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class SearchPageComponent {
+
+  public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+
+  constructor( private heroesService: HeroesService ){}
+
+  searchHero(){
+    const value: string = this.searchInput.value || '';
+
+    this.heroesService.getSuggestions(value)
+    .subscribe( heroes => this.heroes = heroes );
+  }
+
+
+
+
 
 }
